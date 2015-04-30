23f75991c73118f453f0244bdd4babf6ae3e3d16
Peter Ballett <peter.ballett@durham.ac.uk>
2015-04-30 16:43:40 +0100

    updated looper scripts

--- chi2/looper.sh
+++ chi2/looper.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-FILENAME=data/angle5.0_noRatio/bestfit.dat
+#mkdir data/angle20.0_noRatio
+FILENAME=data/angle20.0_noRatio/bestfit.dat
 
 echo > "$FILENAME"
-echo > logfile.dat
-for ms in `ls ../decay/data/ | awk 'FS="_" {print $1}' | uniq | tail -n63 | sort -r -n | paste -sd ' ' -`
-do
-#ms=0.0210
 
+#for ms in `ls ../decay/data/ | awk 'FS="_" {print $1}' | uniq | tail -n63 | sort -r -n | paste -sd ' ' -`
+for ms in `seq 0.0590 -0.0020 0.0010` 
+do
 #if [ `echo "$ms < 0.1110" | bc -l` -eq 1 ]
 #then
 
-for mZ in `ls ../decay/data | grep $ms"_" | awk 'FS="_" {print $2}' | sed 's/.dat//g' | uniq | sort -rn | paste -sd ' ' -`
+#for mZ in `ls ../decay/data | grep $ms"_" | awk 'FS="_" {print $2}' | sed 's/.dat//g' | uniq | sort -rn | paste -sd ' ' -`
+for mZ in `seq 0.1200 0.0200 0.9800`
 do
+#if [ `echo "$mZ < 0.7200" | bc -l` -eq 1 ]
+#then
+
 	if [ `echo "$ms < $mZ" | bc -l` -eq 1 ];
 	then 
-		echo $ms $mZ >> logfile.dat
 		echo $ms $mZ
-		./stats -m $ms -Z $mZ -c 5.0 -P >> "$FILENAME"
-#		./stats $ms $mZ 0.0 180.0
+		./stats -m $ms -Z $mZ -c 20.0 -P >> "$FILENAME"
 			
 	fi
+#fi
 done
 	echo  >> "$FILENAME"
 	echo
--- decay/looper.sh
+++ decay/looper.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo > logfile.dat
+#echo > logfile.dat
 for ms in `ls ../MC/MC_* | awk 'FS="_" {print $2}' | uniq | sort -n | paste -sd ' ' -`
 do
 
-for mZ in `ls ../MC | grep "MC_"$ms | awk 'FS="_" {print $3}' | sed 's/.dat//g' | uniq | sort -rn | paste -sd ' ' -`
-do
+if [ `echo "$ms-0.038 > 0" | bc -l` -eq 1 ]
+then
+
+	for mZ in `ls ../MC/MC_* | awk 'FS="_" {print $3}' | tail -n44 | sed 's/.dat//g' | sort -rn`
+	do
 
 	START=`date +%s`
 	echo $ms $mZ
@@ -15,7 +18,8 @@ do
 	echo $ms" "$mZ" "$RUNTIME >> logfile.dat
 	echo RUNTIME=$RUNTIME
 
-done
+	done
+fi
 
 done 
 
